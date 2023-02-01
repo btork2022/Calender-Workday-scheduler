2164f8fc5d5ff75308cebbbd05ccfdf1446ae4f3
btork2022 <bentork2010@gmail.com>
2023-02-01 11:11:53 +0000

    time of day displayed as well as 15sec hourly update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-  $(document).ready(function() {
+  $(document).ready(function(){
 
     // listen to save-button clicks
 
@@ -17,7 +17,7 @@
         $(".notification").addClass('show');
 
         setTimeout(function(){
-            $("notification").removeClass("show");
+            $(".notification").removeClass("show");
         }, 5000)
     })
 
@@ -25,13 +25,42 @@
     function hourUpdate(){
         let currentHour = moment().hours();
         
-        $(".time-block").each(function(){
-            let blockHour = parseInt($(this).attr("id").split("-")[9]);
-            alert(blockHour);
-        })
+        for(let i = 0; i < $(".time-block").length; i++){
+            let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1])
+            console.log(hour)
+            console.log(currentHour)
+            if(hour < currentHour) {
+                $(".time-block")[i].classList.add("past")
+            }  else if(hour === currentHour){
+                $(".time-block")[i].classList.add("past")
+                $(".time-block")[i].classList.remove("present")
+            }  else {
+                $(".time-block")[i].classList.remove("past")
+                $(".time-block")[i].classList.remove("present")
+                $(".time-block")[i].classList.add("future")
+            }
+        }
+            
 
     }
 
+    hourUpdate();
+
+
+    let interval = setInterval(hourUpdate, 15000);
+
+    $("#9-hour .description").val(localStorage.getItem("9-hour"))
+    $("#10-hour .description").val(localStorage.getItem("10-hour"))
+    $("#11-hour .description").val(localStorage.getItem("11-hour"))
+    $("#12-hour .description").val(localStorage.getItem("12-hour"))
+    $("#13-hour .description").val(localStorage.getItem("13-hour"))
+    $("#14-hour .description").val(localStorage.getItem("14-hour"))
+    $("#15-hour .description").val(localStorage.getItem("15-hour"))
+    $("#16-hour .description").val(localStorage.getItem("16-hour"))
+    $("#17-hour .description").val(localStorage.getItem("17-hour"))
+
+
+    $("#currentDay").text(moment().format("dddd, MMMM, Do"))
 
 
   })
